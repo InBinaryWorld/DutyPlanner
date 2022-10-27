c0531a5d7f46076395de275d4f969615641fa188
Krzysztof Szafraniak <k.szafraniak1@wp.pl>
2022-10-27 10:49:55 +0200

    UI/UX, Name validator update

--- src/app/core/base/validators.ts
+++ src/app/core/base/validators.ts
@@ -2,7 +2,7 @@ import { ValidatorFn, Validators } from '@angular/forms';
 
 export abstract class DPValidators {
   public static shortName(): ValidatorFn {
-    return Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(12)]) as ValidatorFn
+    return Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(14)]) as ValidatorFn
   }
 
   public static place(): ValidatorFn {
